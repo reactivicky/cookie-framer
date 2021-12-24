ab7c00ff35a276fad5fcfbdbb4269af7d8a96e2a
VigneshInforma <vignesh.kumar@informa.com>
2021-12-24 16:32:45 +0530

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,64 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import { motion } from 'framer-motion'
+import CookieSVG from './components/CookieSVG'
+import { useState } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const container = {
+	hidden: {
+		scale: 0,
+	},
+	visible: (show: boolean) => {
+		return {
+			scale: 1,
+			transition: {
+				duration: show ? 1 : 0.5,
+				when: 'beforeChildren',
+				...(show && { type: 'spring', stiffness: 200 }),
+			},
+			opacity: show ? 1 : 0,
+			y: show ? 0 : 100,
+		}
+	},
 }
 
-export default App;
+const text = {
+	hidden: {
+		x: 30,
+		opacity: 0,
+	},
+	visible: {
+		x: 0,
+		opacity: 1,
+	},
+}
+
+function App() {
+	const [show, setShow] = useState<boolean>(true)
+	return (
+		<motion.div
+			variants={container}
+			custom={show}
+			initial='hidden'
+			animate='visible'
+			className='cookie-container'
+		>
+			<CookieSVG />
+			<motion.div
+				variants={text}
+				transition={{
+					duration: 1,
+				}}
+				className='text'
+			>
+				<h2 className='cookie-title'>Cookie Policy</h2>
+				<p className='cookie-subtitle'>
+					We use analytical cookies (yum) to make your experience on this
+					website better.
+				</p>
+				<button onClick={() => setShow(false)}>Okay, got it</button>
+			</motion.div>
+		</motion.div>
+	)
+}
+
+export default App
